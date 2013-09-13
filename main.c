5d3c1442fec2583b777f090d4b639f934f413538
ryankung <ryankung@ieee.org>
2013-09-14 00:06:14 +0800

    rm font config

--- main.c
+++ main.c
@@ -1,47 +1,16 @@
 #include <stdio.h>
 #include <OpenGL/gl.h>
 #include <GLUT/glut.h>
-#include <ft2build.h>
-#include FT_FREETYPE_H
-
 
 void initGL(int, char**);
 void initView();
 void mainViewController();
-FT_Face initFont();
-void setPixel(FT_Face, int, int);
 
 int main(int argc, char** argv) {
-    FT_Face font = initFont();
-    setPixel(font, 0, 16*64);
     initGL(argc, argv);
     glutDisplayFunc(mainViewController);
     glutMainLoop();
     return 0;
-}
-
-//Doc: http://www.freetype.org/freetype2/docs/tutorial/step1.html
-
-FT_Face initFont() {
-    FT_Library library;
-    FT_Face face;
-    int error = FT_Init_FreeType( &library );
-    if ( error ) {
-        perror("Can't find or open the file");
-    };
-    error = FT_New_Face( library, "./fonts/Hei.ttf", 0, &face );
-    if ( error == FT_Err_Unknown_File_Format ) {
-    } else if ( error ) {
-        perror("Can't read the file or the file is broken");
-    }
-    return face;
-}
-
-void setPixel(FT_Face face, int width, int height) {
-    int horizReso = 300,
-        vertiReso = 300;
-    if (FT_Set_Char_Size(face, width, height, horizReso, vertiReso))
-        perror("Set piex failed");
 }
 
 void initGL(int argc, char** argv) {
